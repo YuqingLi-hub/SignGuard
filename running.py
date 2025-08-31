e8b5d4a6844a9c82a7844f20a9ddc46b708a5cee
Yuqing_nt <li.y.q11092@gmail.com>
2025-08-31 16:30:02 +1000

    looking for a solution to  updating the weights

--- running.py
+++ running.py
@@ -23,23 +23,29 @@ class Worker():
         attack = args.attack
         global_grads = None
         masks = args.masks if hasattr(args, 'masks') else [0,1000]
-        # print(tools.get_parameter_values(model))
         # print('embedding locations Watermark masks: ',masks)
+        # print(tools.get_parameter_values(model))
         Watermark = None
-        # detecting and restoring watermark
         if watermark:
             global_grads = tools.get_parameter_values(model)
             _global_grads = copy.deepcopy(global_grads)
             Watermark = QIM(args.delta)
             ################# args.alpha for now #################
             _global_grads,message = detect_recover_on_position(masks=masks,whole_grads=_global_grads,Watermark=Watermark,args=args)
-            print(global_grads[masks[0]:masks[1]][:10],'recovered',_global_grads[masks[0]:masks[1]][:10])
+            # print(global_grads[masks[0]:masks[1]][:10],'recovered',_global_grads[masks[0]:masks[1]][:10])
+            # print(global_grads[masks[0]:masks[1]][:10],Watermark.x[:10],'recovered',_global_grads[masks[0]:masks[1]][:10])
             # err = torch.mean(torch.abs(_global_grads[masks[0]:masks[1]] 
             #                - torch.tensor(Watermark.x, dtype=torch.float32).to(device)))
             # print("Average recovery error:", err.item())
             # print('Client Recovery completed: ', torch.allclose(_global_grads[masks[0]:masks[1]],torch.tensor(Watermark.x,dtype=torch.float32).to(device)))
-            vector_to_parameters(_global_grads, model.parameters())
+            for param in model.parameters():
+                param.requires_grad_(True)
+
+            # Update model with recovered parameters
+            with torch.no_grad():
+                vector_to_parameters(_global_grads, model.parameters())
         model.train()
+        before_train = tools.get_parameter_values(model)
         criterion = nn.CrossEntropyLoss()
         images, labels = next(train_loader)
         images, labels = images.to(device), labels.to(device)
@@ -47,15 +53,22 @@ class Worker():
         outputs = model(images)
         loss = criterion(outputs, labels)
         loss.backward()
-        user_grad = tools.get_parameter_values(model)
-        self.previous_grad = user_grad
+        # 🔑 UPDATE PARAMETERS HERE
+        optimizer.step()
 
+        # ✅ Now get updated parameters (new weights)
+        user_grad = tools.get_parameter_values(model)
+        print('Not Updating =', torch.allclose(user_grad, before_train))
+        print('Updated params (first 10):', user_grad[masks[0]:masks[1]][:10])
+        self.previous_grad = user_grad
         # embedding watermark
         if watermark:
             _user_grad = copy.deepcopy(user_grad)
             user_grad_w = embedding_watermark_on_position(masks,_user_grad,Watermark,message,args)
+            print('Watermarked',user_grad_w[masks[0]:masks[1]][:10])
         # print('Client Watermarking completed: ', torch.allclose(user_grad,user_grad_w))
         print()
+        
         return user_grad_w, loss.item(), args.alpha
 
     def byzantineWorker(self, model, train_loader, optimizer, args, watermark=False):
@@ -72,29 +85,43 @@ class Worker():
             Watermark = QIM(args.delta)
             ################# args.alpha for now #################
             _global_grads,message = detect_recover_on_position(masks=masks,whole_grads=_global_grads,Watermark=Watermark,args=args)
-            print(global_grads[masks[0]:masks[1]][:10],'recovered',_global_grads[masks[0]:masks[1]][:10])
+            # print(global_grads[masks[0]:masks[1]][:10],'recovered',_global_grads[masks[0]:masks[1]][:10])
             # print(global_grads[masks[0]:masks[1]][:10],Watermark.x[:10],'recovered',_global_grads[masks[0]:masks[1]][:10])
             # err = torch.mean(torch.abs(_global_grads[masks[0]:masks[1]] 
             #                - torch.tensor(Watermark.x, dtype=torch.float32).to(device)))
             # print("Average recovery error:", err.item())
             # print('Client Recovery completed: ', torch.allclose(_global_grads[masks[0]:masks[1]],torch.tensor(Watermark.x,dtype=torch.float32).to(device)))
-            vector_to_parameters(_global_grads, model.parameters())
+            for param in model.parameters():
+                param.requires_grad_(True)
+
+            # Update model with recovered parameters
+            with torch.no_grad():
+                vector_to_parameters(_global_grads, model.parameters())
         model.train()
+        before_train = tools.get_parameter_values(model)
         criterion = nn.CrossEntropyLoss()
         images, labels = next(train_loader)
+        images, labels = images.to(device), labels.to(device)
         if attack=='label_flip':
             labels = 9 - labels
-        images, labels = images.to(device), labels.to(device)
         optimizer.zero_grad()
         outputs = model(images)
         loss = criterion(outputs, labels)
         loss.backward()
+
+        # 🔑 UPDATE PARAMETERS HERE
+        optimizer.step()
+
+        # ✅ Now get updated parameters (new weights)
         user_grad = tools.get_parameter_values(model)
+        print("Weights updated?", not torch.allclose(before_train, user_grad, rtol=1e-5, atol=1e-6))
+        print('Updated params (first 10):', user_grad[masks[0]:masks[1]][:10])
         self.previous_grad = user_grad
         # embedding watermark
         if watermark:
             _user_grad = copy.deepcopy(user_grad)
             user_grad_w = embedding_watermark_on_position(masks,_user_grad,Watermark,message,args)
+            print('Watermarked',user_grad_w[masks[0]:masks[1]][:10])
         # print('Client Watermarking completed: ', torch.allclose(user_grad,user_grad_w))
         print()
         
@@ -115,3 +142,13 @@ def test_classification(device, model, test_loader):
     # print('Test Accuracy: %.2f %%' % (acc))
 
     return acc
+
+
+def get_parameter_values(model):
+    """ Get (optionally make each parameter's gradient) a reference to the flat gradient.
+    Returns:
+      Flat gradient (by reference: future calls to 'set_gradient' will modify it)
+    """
+
+    parameter = torch.cat([torch.reshape(param.data, (-1,)) for param in model.parameters()])
+    return parameter
