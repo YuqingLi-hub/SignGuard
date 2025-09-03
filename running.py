fix the gradient accumulating problem, try use local optimizer etc.

running.py
@@ -30,13 +30,10 @@ def all_worker(model, train_loader, optimizer, args, water=False,malicious=True)
     Qim = None
     # recovered_grads = None
     global_param = tools.get_parameter_values(model)
-    logger.info(f'Client Received parameters {global_param[masks[0]:masks[1]][:10]}')
+    logger.debug(f'Client Received parameters {global_param[masks[0]:masks[1]][:10]}')
     # ------------------ Updating model parameters - Watermark detection & recovery ------------------
     if water:
-        # Get current parameters
-        # global_grads = tools.get_gradient_values(model)
         # print('Received gradients',global_grads[masks[0]:masks[1]][:10])
-        
         Qim = QIM(delta)
 
         # Recover watermark
@@ -48,16 +45,16 @@ def all_worker(model, train_loader, optimizer, args, water=False,malicious=True)
             k=k,
             model=model
         )
-        logger.info(f'Client Recovered params {global_param[masks[0]:masks[1]][:10]}')
+        logger.debug(f'Client Recovered params {global_param[masks[0]:masks[1]][:10]}')
         # print('Recovered gradients',recovered_grads[masks[0]:masks[1]][:10])
-        with torch.no_grad():
-            start = 0
-            for p in model.parameters():
-                numel = p.numel()
-                g = global_param[start:start+numel].view_as(p)
-                p = g
-                start += numel
-        logger.info(f'Updated recovered params {tools.get_parameter_values(model)[masks[0]:masks[1]][:10]}')
+        # with torch.no_grad():
+        #     start = 0
+        #     for p in model.parameters():
+        #         numel = p.numel()
+        #         g = global_param[start:start+numel].view_as(p)
+        #         p = g
+        #         start += numel
+        logger.debug(f'Updated recovered params {tools.get_parameter_values(model)[masks[0]:masks[1]][:10]}')
     # ------------------ Training step ------------------
     model.train()
     criterion = nn.CrossEntropyLoss()
@@ -67,14 +64,11 @@ def all_worker(model, train_loader, optimizer, args, water=False,malicious=True)
         labels = 9 - labels
     images, labels = images.to(device), labels.to(device)
 
-    # IMPORTANT: zero local grads
+    # zero local grads
     for p in model.parameters():
         if p.grad is not None:
             p.grad.zero_()
 
-    # if recovered_grads is not None:
-    #     # ✅ Use recovered gradients instead of backward()
-    # tools.update_gradient(global_param, model)
     # optimizer.step()
     outputs = model(images)
     loss = criterion(outputs, labels)
@@ -82,7 +76,7 @@ def all_worker(model, train_loader, optimizer, args, water=False,malicious=True)
 
     # Get updated grads/params
     updated_grads = tools.get_gradient_values(model)
-    logger.info(f'Updated grads (first 10): {updated_grads[masks[0]:masks[1]][:10]}')
+    logger.debug(f'Updated grads (first 10): {updated_grads[masks[0]:masks[1]][:10]}')
 
     # ------------------ Embed watermark if required ------------------
     if water:
@@ -90,7 +84,7 @@ def all_worker(model, train_loader, optimizer, args, water=False,malicious=True)
         user_grad_w = embedding_watermark_on_position(
             masks, _user_param, Qim, message, alpha=alpha,k=k, model=model
         )
-        logger.info(f'Watermarked (first 10): {user_grad_w[masks[0]:masks[1]][:10]}')
+        logger.debug(f'Watermarked (first 10): {user_grad_w[masks[0]:masks[1]][:10]}')
     else:
         user_grad_w = updated_grads
   

tools.py
@@ -3,7 +3,7 @@ import torch
 import types
 import math
 import numpy as np
-
+from torch.nn.utils import vector_to_parameters
 # --------------------------------------------------------------------- #
 # Gradient access
 
@@ -285,11 +285,12 @@ def embedding_watermark_on_position(masks,whole_grads,Watermark,message,alpha,k,
     # If model is provided, update the actual model parameters in-place
     if model is not None:
         with torch.no_grad():
-            start = 0
-            for p in model.parameters():
-                numel = p.numel()
-                p.data.copy_(whole_grads[start:start+numel].view_as(p))
-                start += numel
+            vector_to_parameters(whole_grads,model.parameters())
+            # start = 0
+            # for p in model.parameters():
+            #     numel = p.numel()
+            #     p.data.copy_(whole_grads[start:start+numel].view_as(p))
+            #     start += numel
     return whole_grads
 
 def detect_recover_on_position(masks,whole_grads,Watermark,alpha,k,model=None):
@@ -306,11 +307,12 @@ def detect_recover_on_position(masks,whole_grads,Watermark,alpha,k,model=None):
     whole_grads[masks[0]:masks[1]].copy_(reconstructed_grad)
     if model is not None:
         with torch.no_grad():
-            start = 0
-            for p in model.parameters():
-                numel = p.numel()
-                p.data.copy_(whole_grads[start:start+numel].view_as(p))
-                start += numel
+            vector_to_parameters(whole_grads,model.parameters())
+            # start = 0
+            # for p in model.parameters():
+            #     numel = p.numel()
+            #     p.data.copy_(whole_grads[start:start+numel].view_as(p))
+            #     start += numel
     return whole_grads, mm.to(device)
 def update_gradient(whole_grads, model):
     with torch.no_grad():